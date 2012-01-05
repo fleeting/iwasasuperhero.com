bbee03f9eacb12bc790417537f2f0456b6fd9786
James Fleeting <twofivethreetwo@gmail.com>
2012-01-04 18:19:18 -0600

    initial commit
    adding v4 template, two test blog posts, moved site over to jekyll (using jekyll bootstrap), linked up sidebar icons, styled pinboard feed in sidebar

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	
+});
+
+var ADAPT_CONFIG = {
+	path: 'css/',
+	dynamic: true,
+	range: [
+		'0px to 480px = 480.css',
+		'480px to 750px = 750.css',
+		'750px to 1000px = 1000.css'
+	]
+};
